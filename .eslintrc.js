d44d05099c15ca6972ea9c261586660c3ffe9c45
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2018-05-04 15:34:31 +0700

    Feature: Create standart config files What i change: * create babel config file * make standart space and tab * create eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 6,
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    jest: true,
+    es6: true
+  },
+  extends: 'airbnb',
+  rules: {
+    // allow async-await
+    'generator-star-spacing': 0,
+    // user defined rules
+    'arrow-parens': [1, 'as-needed'],
+    'no-undef': 0,
+    'space-before-function-paren': ['error', 'always'],
+    quotes: [1, 'single'],
+    semi: [1, 'never'],
+    'import/no-extraneous-dependencies': [2, { devDependencies: true }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'max-len': [
+      2,
+      {
+        code: 100
+      }
+    ],
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        vue: 'never'
+      }
+    ],
+    'comma-dangle': ['error', 'never']
+  }
+}
